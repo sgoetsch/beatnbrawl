1e92e4436991f06fa9212b990b42db9b5f2021a4
Sebastian Goetsch <pokerface@quadspot.de>
2014-08-05 16:26:29 +0200

    Frameworking

---
+++ core/src/de/quadspot/beatnbrawl/GameManager.java
@@ -0,0 +1,9 @@
+package de.quadspot.beatnbrawl;
+
+import com.badlogic.ashley.core.EntitySystem;
+
+/**
+ * Created by goetsch on 05.08.14.
+ */
+public class GameManager extends EntitySystem {
+}
--- core/src/de/quadspot/beatnbrawl/beatnbrawl.java
+++ core/src/de/quadspot/beatnbrawl/beatnbrawl.java
@@ -1,27 +1,53 @@
 package de.quadspot.beatnbrawl;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class beatnbrawl extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	//SpriteBatch batch;
+	//Texture img;
+    Engine engine;
+    OrthographicCamera camera;
+    Float delta;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+/*		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");*/
+        delta = Gdx.graphics.getDeltaTime();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        engine = new Engine();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+/*		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
+		batch.end();*/
+
+        engine.update(delta);
 	}
+
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void dispose () {
+    }
 }
