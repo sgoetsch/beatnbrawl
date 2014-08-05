ab439e76ff2652ec84650538e69a8e94ed5329a3
Sebastian Goetsch <pokerface@quadspot.de>
2014-08-05 22:04:05 +0200

    First Steps

--- core/src/de/quadspot/beatnbrawl/systems/InputSystem.java
+++ core/src/de/quadspot/beatnbrawl/systems/InputSystem.java
@@ -1,9 +1,17 @@
 package de.quadspot.beatnbrawl.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 /**
  * Created by goetsch on 05.08.14.
  */
-public class InputSystem extends EntitySystem imple{
+public class InputSystem extends EntitySystem{
+    private Touchpad touchpad;
+    private Touchpad.TouchpadStyle touchpadStyle;
+    private Skin touchpadSkin;
+    private Drawable touchBackground;
+    private Drawable touchKnob;
 }
