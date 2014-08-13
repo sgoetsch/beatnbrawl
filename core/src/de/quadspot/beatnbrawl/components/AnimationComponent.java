822d3e476f8cc9623b4ee9428b8e160667addbb1
Sebastian Goetsch <pokerface@quadspot.de>
2014-08-13 17:35:43 +0200

    States

---
+++ core/src/de/quadspot/beatnbrawl/components/AnimationComponent.java
@@ -0,0 +1,9 @@
+package de.quadspot.beatnbrawl.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by goetsch on 13.08.14.
+ */
+public class AnimationComponent extends Component {
+}
