f7073b6f4a0dc281323c3146b2a9525d1789a640
Sebastian Goetsch <pokerface@quadspot.de>
2014-10-11 21:20:31 +0200

    continued...

--- core/src/de/quadspot/beatnbrawl/screens/MainMenuScreen.java
+++ core/src/de/quadspot/beatnbrawl/screens/MainMenuScreen.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -20,6 +21,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import de.quadspot.beatnbrawl.beatnbrawl;
+
+//http://stackoverflow.com/questions/14675007/how-to-draw-smooth-text-in-libgdx
 
 /**
  *
@@ -29,40 +32,12 @@ public class MainMenuScreen implements Screen {
     final beatnbrawl game;
 
     OrthographicCamera camera;
-    private final TextButton startGameButton;
+    private TextButton startGameButton;
     Stage stage;
 
     public MainMenuScreen(final beatnbrawl gam) {
         game = gam;
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-        Sprite sp = new Sprite(new Texture(Gdx.files.internal("button.png")));
-        sp.setBounds(1500,1500, 100, 100);
-        SpriteDrawable sprite = new SpriteDrawable(sp);
-        
-        TextButtonStyle style = new TextButtonStyle(sprite,sprite,sprite,game.font);
-
-        startGameButton = new TextButton( "Start game",  style);
-        
-        
-        
-        startGameButton.addListener( new InputListener () {
-            public boolean touchDown (int x, int y, int pointer, int button) {
-             game.setScreen(new GameScreen(game));
-             return true; // return true to indicate the event was handled
-            }
-        } );
-        
-        
         stage = new Stage();
-        stage.addActor(startGameButton);
-        stage.addListener( new InputListener () {
-            public boolean touchDown (int x, int y, int pointer, int button) {
-             game.setScreen(new GameScreen(game));
-             return true; // return true to indicate the event was handled
-            }
-        } );
 
     }
 
@@ -71,23 +46,23 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-
-        camera.update();
+/*
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
         game.font.draw(game.batch, "Welcome to Turtles in Time!!! ", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);*/
+        stage.act();
         stage.draw();
-        game.batch.end();
+        //game.batch.end();
         
         
         
 
-        if (Gdx.input.isTouched()) {
+/*        if (Gdx.input.isTouched()) {
             game.setScreen(new GameScreen(game));
             dispose();
-        }    
+        } */
     }
 
     @Override
@@ -96,6 +71,30 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
+        Sprite sp = new Sprite(new Texture(Gdx.files.internal("button.png")));
+        sp.setBounds(0,0, 1000, 100);
+        SpriteDrawable sprite = new SpriteDrawable(sp);
+
+
+
+        TextButtonStyle style = new TextButtonStyle(sprite,sprite,sprite,game.font);
+
+        startGameButton = new TextButton( "Start game",  style);
+
+
+
+        startGameButton.addListener( new InputListener () {
+
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new GameScreen(game));
+                return true; // return true to indicate the event was handled
+            }
+        } );
+
+
+        stage.addActor(startGameButton);
+        Gdx.input.setInputProcessor(stage);
+
     }
 
     @Override
